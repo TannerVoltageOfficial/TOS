bb5b7ea376e838d1b40ede59c98e1ca68206a684
Tanner Voltage! <tannervoltage@gmail.com>
2025-09-19 20:02:20 -0700

    update kinda

--- main.ts
+++ main.ts
@@ -1,24 +1,30 @@
-/**
- * This is the main file for your project.
- *
- * Create images, tilemaps, animations, and songs using the
- * asset explorer in VS Code. You can reference those assets
- * using the tagged templates on the assets namespace:
- *
- *     assets.image`myImageName`
- *     assets.tilemap`myTilemapName`
- *     assets.tile`myTileName`
- *     assets.animation`myAnimationName`
- *     assets.song`mySongName`
- *
- * New to MakeCode Arcade? Try creating a new project using one
- * of the templates to learn about Sprites, Tilemaps, Animations,
- * and more! Or check out the reference docs here:
- *
- * https://arcade.makecode.com/reference
- */
+/*
+T-OS
+Tanner's Operating System
+Made by TannerVoltageOfficial on GitHub
+*/
+// --START USELESS VARIABLES--
+let homeMenu
+let version = "0.0.1"
+let aboutText = "T-OS V" + version + "."
+// --START USELESS FUNCTIONS--
+function showAbout() {
+    game.splash("About T-OS", aboutText)
+}
+function homeSelect() {
 
+}
+function showHome() {
+    homeMenu = miniMenu.createMenu(miniMenu.createMenuItem("About T-OS"), miniMenu.createMenuItem("Close"))
+    homeMenu.onButtonPressed(ControllerButton.A,homeSelect)
+}
+// --START USELESS CODE--
+scene.setBackgroundColor(2)
+showHome()
+/* onupdate closed for maintnence
+// --START USELESS ONUPDATE--
 game.onUpdate(() => {
     // Code in this function will run once per frame. MakeCode
     // Arcade games run at 30 FPS
 });
+*/
