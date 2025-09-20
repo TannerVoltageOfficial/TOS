311caaa8f2670d683bb01408c8944bd3f4b99dfa
Tanner Voltage! <tannervoltage@gmail.com>
2025-09-20 04:31:25 +0000

    update license and make sections

--- main.ts
+++ main.ts
@@ -3,28 +3,24 @@ T-OS
 Tanner's Operating System
 Made by TannerVoltageOfficial on GitHub
 */
-// --START USELESS VARIABLES--
+//#region variables
 let homeMenu
 let version = "0.0.1"
 let aboutText = "T-OS V" + version + "."
-// --START USELESS FUNCTIONS--
+//#endregion
+//#region functions
 function showAbout() {
     game.splash("About T-OS", aboutText)
 }
-function homeSelect() {
+function homeSelect(selection:string) {
 
 }
 function showHome() {
     homeMenu = miniMenu.createMenu(miniMenu.createMenuItem("About T-OS"), miniMenu.createMenuItem("Close"))
     homeMenu.onButtonPressed(controller.A,homeSelect)
 }
-// --START USELESS CODE--
+//#endregion
+//#region main
 scene.setBackgroundColor(2)
 showHome()
-/* onupdate closed for maintnence
-// --START USELESS ONUPDATE--
-game.onUpdate(() => {
-    // Code in this function will run once per frame. MakeCode
-    // Arcade games run at 30 FPS
-});
-*/
+//#endregion
